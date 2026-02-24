e2abcbfec5fc167fa896f24aaca7719476c56c05
Roshan Reji K <roshan.reji@oneteamus.com>
2026-02-24 15:42:04 +0530

    cd

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const customJestConfig = {
+    setupFilesAfterSetup: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
